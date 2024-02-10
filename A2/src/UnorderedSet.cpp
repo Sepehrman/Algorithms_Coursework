e7de14d08fbf0f5c94eb3c9975dcc1a02c0a5195
Sepehrman <sepehrman@icloud.com>
2024-02-09 19:10:28 -0800

    Added updates to insert & size

--- A2/src/UnorderedSet.cpp
+++ A2/src/UnorderedSet.cpp
@@ -4,26 +4,28 @@
 
 #include "../include/UnorderedSet.h"
 
+using namespace std;
 
 template<typename Key>
 UnorderedSet<Key>::UnorderedSet() {
     root = nullptr;
-
+    setSize = 0;
 }
 
 template<typename Key>
 UnorderedSet<Key>::~UnorderedSet() {
-    root = nullptr;
-
-}
-
-template<typename Key>
-typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::end() const {
-    return UnorderedSet<Key>::Iterator(nullptr);
+    clear();
 }
 
 template<typename Key>
 typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::begin() const {
+    return UnorderedSet<Key>::Iterator(root);
+}
+
+
+
+template<typename Key>
+typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::end() const {
     return UnorderedSet<Key>::Iterator(nullptr);
 }
 
@@ -31,23 +33,49 @@ typename UnorderedSet<Key>::Iterator UnorderedSet<Key>::begin() const {
 template<typename Key>
 bool UnorderedSet<Key>::insert(const Key &key) {
 
+
     if (root == nullptr) {
         root = new Node<Key>(key);
         root->color = Color::BLACK;
+        setSize++;
         return true;
     }
 
-    if (root->key > key) {
-        return insert(root->left);
+    if (key < root->key) {
+        root = root->left;
+        insert(key);
+        // WORKING SAMPLE
+//        root->left = new Node<Key>(key);
+//        root->left->parent = root;
+//        return true;
     }
 
-    if (root->key < key) {
-        return insert(root->left);
+    if (key > root->key) {
+        root = root->right;
     }
+
+    return true;
+//    if (root->key > key) {
+//        root->left;
+//        return insert();
+//    }
+//
+//    if (root->key < key) {
+//        return insert(root->left);
+//    }
 }
 
 template<typename Key>
 bool UnorderedSet<Key>::search(const Key &key) const {
+
+//    if (key == nullptr) {
+//        return false;
+//    }
+//
+//    if (key < root->key) {
+//
+//    }
+//
     return false;
 }
 
@@ -63,7 +91,7 @@ void UnorderedSet<Key>::clear() {
 
 template<typename Key>
 size_t UnorderedSet<Key>::size() const {
-    return 0;
+    return setSize;
 }
 
 template<typename Key>
@@ -73,7 +101,7 @@ void UnorderedSet<Key>::updateSize() {
 
 template<typename Key>
 size_t UnorderedSet<Key>::getSize(Node<Key> *node) const {
-    return 0;
+    return setSize;
 }
 
 template<typename Key>
