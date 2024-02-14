c7f1b368b0e0cc2c90cf51dda383ab4d4d33eba5
Sepehr Mansouri <Sepehrman@icloud.com>
2024-02-14 10:57:41 -0800

    Update made to HashTable.cpp

--- A2/src/HashTable.cpp
+++ A2/src/HashTable.cpp
@@ -4,70 +4,74 @@
  */
 
 
-//#include "../include/HashTable.h"
-//
-//template<typename KeyType, typename ValueType>
-//HashTable<KeyType, ValueType>::HashTable(unsigned int size, double threshold) {
-//
-//}
-//
-//template<typename KeyType, typename ValueType>
-//HashTable::Iterator HashTable<KeyType, ValueType>::begin() {
-//    return HashTable::Iterator(__gnu_cxx::__normal_iterator(), __gnu_cxx::__normal_iterator());
-//}
-//
-//template<typename KeyType, typename ValueType>
-//HashTable::Iterator HashTable<KeyType, ValueType>::end() {
-//    return HashTable::Iterator(__gnu_cxx::__normal_iterator(), __gnu_cxx::__normal_iterator());
-//}
-//
-//template<typename KeyType, typename ValueType>
-//ValueType &HashTable<KeyType, ValueType>::operator[](const KeyType &key) {
-//    return <#initializer#>;
-//}
-//
-//template<typename KeyType, typename ValueType>
-//void HashTable<KeyType, ValueType>::updateValueForKey(const KeyType &key, ValueType newValue) {
-//
-//}
-//
-//template<typename KeyType, typename ValueType>
-//void HashTable<KeyType, ValueType>::insert(const KeyType &key, const ValueType &value) {
-//
-//}
-//
-//template<typename KeyType, typename ValueType>
-//ValueType *HashTable<KeyType, ValueType>::search(const KeyType &key) {
-//    return nullptr;
-//}
-//
-//template<typename KeyType, typename ValueType>
-//bool HashTable<KeyType, ValueType>::remove(const KeyType &key) {
-//    return false;
-//}
-//
-//template<typename KeyType, typename ValueType>
-//void HashTable<KeyType, ValueType>::clear() {
-//
-//}
-//
-//template<typename KeyType, typename ValueType>
-//unsigned int HashTable<KeyType, ValueType>::size() const {
-//    return 0;
-//}
-//
-//template<typename KeyType, typename ValueType>
-//double HashTable<KeyType, ValueType>::loadFactor() const {
-//    return 0;
-//}
-//
-//template<typename KeyType, typename ValueType>
-//unsigned int
-//HashTable<KeyType, ValueType>::findFreeSlot(vector<Bucket> &cTable, unsigned int startIndex, unsigned int &currentHop) {
-//    return 0;
-//}
-//
-//template<typename KeyType, typename ValueType>
-//void HashTable<KeyType, ValueType>::rehash() {
-//
-//}
+#include "../include/HashTable.h"
+
+template<typename KeyType, typename ValueType>
+HashTable<KeyType, ValueType>::HashTable(unsigned int size, double threshold) {
+    hashTable = std::vector<Bucket>();
+    tableSize = size;
+    loadFactorThreshold = threshold;
+}
+
+
+template<typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::updateValueForKey(const KeyType &key, ValueType newValue) {
+
+}
+
+template<typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::insert(const KeyType &key, const ValueType &value) {
+
+}
+
+template<typename KeyType, typename ValueType>
+ValueType *HashTable<KeyType, ValueType>::search(const KeyType &key) {
+    return nullptr;
+}
+
+template<typename KeyType, typename ValueType>
+bool HashTable<KeyType, ValueType>::remove(const KeyType &key) {
+    return false;
+}
+
+template<typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::clear() {
+
+}
+
+template<typename KeyType, typename ValueType>
+unsigned int HashTable<KeyType, ValueType>::size() const {
+    return 0;
+}
+
+template<typename KeyType, typename ValueType>
+double HashTable<KeyType, ValueType>::loadFactor() const {
+    return 0;
+}
+
+template<typename KeyType, typename ValueType>
+unsigned int
+HashTable<KeyType, ValueType>::findFreeSlot(vector<Bucket> &cTable, unsigned int startIndex, unsigned int &currentHop) {
+    return 0;
+}
+
+template<typename KeyType, typename ValueType>
+void HashTable<KeyType, ValueType>::rehash() {
+
+}
+
+template <typename KeyType, typename ValueType>
+ValueType& HashTable<KeyType, ValueType>::operator[](const KeyType& key) {
+    return hashTable[0].value;
+}
+
+template <typename KeyType, typename ValueType>
+typename HashTable<KeyType, ValueType>::Iterator HashTable<KeyType, ValueType>::begin() {
+    return Iterator(hashTable.begin(), hashTable.end());
+}
+
+// Iterator end function
+template <typename KeyType, typename ValueType>
+typename HashTable<KeyType, ValueType>::Iterator HashTable<KeyType, ValueType>::end() {
+    return Iterator(hashTable.end(), hashTable.end());
+}
