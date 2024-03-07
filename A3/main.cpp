abdbd34676bd7c94e4ed19608fc486876164f309
Sepehr Mansouri <Sepehrman@icloud.com>
2024-03-07 12:07:18 -0800

    Mergesort added

--- A3/main.cpp
+++ A3/main.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 #include <cstdlib>
 #include "tests/MergeSortTests.h"
-#include "tests/RadixSortTests.h"
-#include "tests/LibraryRestructuringTests.h"
+//#include "tests/RadixSortTests.h"
+//#include "tests/LibraryRestructuringTests.h"
 #include "include/LExceptions.h"
 /*
  * This is the driver file which directs the project on testing different modules.
@@ -23,10 +23,10 @@ int main(int argc, char* argv[]) {
         case 0: // Testing Assignment 2:
             std::cout << ">> MergeSort:\t\t\t\t\t\t";
             mergeSortTests();
-            std::cout << ">> RadixSort:\t\t\t\t\t\t";
-            radixSortTests();
-            std::cout << ">> Library Restructuring System: \t";
-            libraryRestructuringTests();
+//            std::cout << ">> RadixSort:\t\t\t\t\t\t";
+//            radixSortTests();
+//            std::cout << ">> Library Restructuring System: \t";
+//            libraryRestructuringTests();
             break;
         default:
             throw std::invalid_argument("Invalid module choice");
---
+++ A3/src/MergeSort.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Sepeh on 2024-03-07.
+//
+
+#include "../include/MergeSort.h"
+
+template<typename T>
+void MergeSort<T>::sort(std::vector<T> &arr) {
+
+}
+
+template<typename T>
+void MergeSort<T>::merge(std::vector<T> &arr, const std::vector<T> &left, const std::vector<T> &right) {
+
+}
