b505c82c5f0b52105dc8c982e203a2e6ddaa8810
Sepehr Mansouri <Sepehrman@icloud.com>
2024-03-12 09:24:04 -0700

    Added documentation

--- A3/src/MergeSort.cpp
+++ A3/src/MergeSort.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 /**
- * Merge sort method. Recursive and Split into smallest size
+ * Merge sort method. Recursively Split into smallest size
  * @param arr an array (Vector) of elements
  */
 template <typename T>
@@ -23,9 +23,9 @@ void MergeSort<T>::sort(std::vector<T>& arr){
     vector<T> left = vector<T>(arr.begin(), arr.begin() + mid);
     vector<T> right = vector<T>(arr.begin() + mid, arr.end());
 
-    sort(left);
-    sort(right);
-    merge(arr, left, right);
+    sort(left); // Recursively sort the left array
+    sort(right); // Recursively sort the right array
+    merge(arr, left, right); // Pointer for the merged array
 
 }
 
@@ -36,7 +36,10 @@ void MergeSort<T>::merge(std::vector<T>& arr, const std::vector<T>& left, const 
     int rightPointer = 0;
     int arrayPointer = 0;
 
+    // Merge the left and right arrays into the main array in sorted order while they are at the given size.
     while(leftPointer < left.size() && rightPointer < right.size()){
+        // Compare if the element of the left or right array are bigger
+        // sets the element to the arrayPointer and increases the arrayPointer & the left/right pointer index by 1
         if(compare(left[leftPointer], right[rightPointer])){
             arr[arrayPointer] = left[leftPointer];
             leftPointer++;
@@ -47,6 +50,7 @@ void MergeSort<T>::merge(std::vector<T>& arr, const std::vector<T>& left, const 
             arrayPointer++;
         }
     }
+    // Copy any remaining elements from the left or right array.
         while (leftPointer < left.size()) {
             arr[arrayPointer++] = left[leftPointer++];
         }
@@ -55,4 +59,3 @@ void MergeSort<T>::merge(std::vector<T>& arr, const std::vector<T>& left, const 
         }
 }
 
-template class MergeSort<int>;
